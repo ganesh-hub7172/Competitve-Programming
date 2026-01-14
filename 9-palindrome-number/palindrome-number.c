c18316d97127acd20b905b335b105fafaf8e0d86
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-01-14 22:25:55 +0530

    Time: 4 ms (25.63%) | Memory: 8 MB (99.54%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    // Negative numbers and numbers ending with 0 (except 0) are not palindromes
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+        return false;
+    }
+
+    int reversedHalf = 0;
+
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + x % 10;
+        x /= 10;
+    }
+
+    // Even length: x == reversedHalf
+    // Odd length:  x == reversedHalf / 10
+    return (x == reversedHalf) || (x == reversedHalf / 10);
+}
