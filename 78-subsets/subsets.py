b33527eae01941c807310e8074122b4b51e6996e
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-01-18 12:11:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (20.30%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums):
+        result = []
+        subset = []
+
+        def backtrack(index):
+            result.append(subset[:])
+
+            for i in range(index, len(nums)):
+                subset.append(nums[i])
+                backtrack(i + 1)
+                subset.pop()
+
+        backtrack(0)
+        return result
