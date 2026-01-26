aab1085c9b47f8bf8b6c611ef995cee13c09c56f
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-01-26 08:57:08 +0530

    Time: 43 ms (36.31%) | Memory: 17.8 MB (88.37%) - LeetSync

---
+++ 134-gas-station/gas-station.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canCompleteCircuit(self, gas, cost):
+        total_gas = 0
+        tank = 0
+        start = 0
+
+        for i in range(len(gas)):
+            diff = gas[i] - cost[i]
+            total_gas += diff
+            tank += diff
+
+            if tank < 0:
+                start = i + 1
+                tank = 0
+
+        return start if total_gas >= 0 else -1
